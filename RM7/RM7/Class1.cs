986b418f1c2d084a41a1bb9f7ceb2edfc52ba0d1
KaiZons <2516361678@qq.com>
2019-03-30 00:22:04 +0800

    KaiZons删除测试代码

--- RM7/RM7/Class1.cs
+++ RM7/RM7/Class1.cs
@@ -4,12 +4,6 @@ namespace RM7
 {
     public class Class1
     {
-        /// <summary>
-        /// 添加备注
-        /// </summary>
-        public void KaiZonsTestGit()
-        {
-
-        }
+        
     }
 }
